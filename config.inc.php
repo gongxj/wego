6e7017403212c036c23ec54f50462c1b9e75baa9
416823980 <416823980@qq.com>
2014-10-26 16:20:09 +0800

    add

---
+++ config.inc.php
@@ -0,0 +1,13 @@
+<?php
+return array (
+  'DB_HOST' => 'localhost',
+  'DB_PWD' => '',
+  'DB_NAME' => 'gong_turn_one',
+  'DB_USER' => 'root',
+  'DB_PORT' => '3306',
+  'DB_PREFIX' => 'gong_',
+  'DEFAULT_THEME' => 'default',
+  'TMPL_ACTION_SUCCESS' => 'public:success',
+  'TMPL_ACTION_ERROR' => 'public:error',
+); 
+?>
---
+++ install/Runtime/Data/install.temp.php
@@ -0,0 +1,13 @@
+<?php
+return array (
+  'db_host' => 'localhost',
+  'db_port' => '3306',
+  'db_user' => 'root',
+  'db_pass' => '',
+  'db_name' => 'gong_turn_one',
+  'db_prefix' => 'gong_',
+  'admin_user' => 'admin',
+  'admin_pass' => 'gongxj1987214',
+  'admin_email' => '416823980@qq.com',
+); 
+?>
